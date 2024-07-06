f80689c924d8b873657f187eee0e61efec30bfc0
MichelleCordovi <michellecordovi@gmail.com>
2024-07-06 17:06:02 -0400

    initialize css and js for start menu

---
+++ src/script.js
@@ -0,0 +1,18 @@
+//NEW GAME MENU START
+const markSelectors = document.getElementsByClassName("mark");
+let selected = markSelectors[0];
+
+// function selectMark() {
+//       selected.querySelector("path").style.fill = "#1F3641";
+//       selected.style.backgroundColor = "#A8BFC9";
+// }
+
+// selectMark();
+
+
+// for (let i = 0; i < markSelectors.length; i++) {
+//       markSelectors[i].onclick = () => {
+//             selected = markSelectors[i];
+//             selectMark();
+//       }
+// }
