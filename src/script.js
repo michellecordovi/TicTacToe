e9272f4fc2d1ff61023e911a355198d91b00d4b4
MichelleCordovi <michellecordovi@gmail.com>
2024-07-06 17:18:34 -0400

    add click event for selecting X or O mark on start menu

--- src/script.js
+++ src/script.js
@@ -2,17 +2,25 @@
 const markSelectors = document.getElementsByClassName("mark");
 let selected = markSelectors[0];
 
-// function selectMark() {
-//       selected.querySelector("path").style.fill = "#1F3641";
-//       selected.style.backgroundColor = "#A8BFC9";
-// }
+function selectMark(selectedMark) {
+      selectedMark.querySelector("path").style.fill = "#1F3641";
+      selectedMark.style.backgroundColor = "#A8BFC9";
+}
 
-// selectMark();
+function unselectMark(originalSelection) {
+      originalSelection.querySelector("path").style.fill = "#A8BFC9";
+      originalSelection.style.backgroundColor = "initial";
+}
+
+selectMark(selected);
 
 
-// for (let i = 0; i < markSelectors.length; i++) {
-//       markSelectors[i].onclick = () => {
-//             selected = markSelectors[i];
-//             selectMark();
-//       }
-// }
+for (let i = 0; i < markSelectors.length; i++) {
+      markSelectors[i].onclick = () => {
+            if (selected !== markSelectors[i]) {
+                  unselectMark(selected);
+                  selected = markSelectors[i];
+                  selectMark(markSelectors[i]);
+            }
+      }
+}
