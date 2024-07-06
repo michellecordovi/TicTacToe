c2bec4d75adbcebf249c0b849f5dd0faeb69d89b
MichelleCordovi <michellecordovi@gmail.com>
2024-07-06 18:33:49 -0400

    add styling to game board

--- src/script.js
+++ src/script.js
@@ -1,26 +1,33 @@
 //NEW GAME MENU START
 const markSelectors = document.getElementsByClassName("mark");
-let selected = markSelectors[0];
+let selectedMark = markSelectors[0];
 
-function selectMark(selectedMark) {
-      selectedMark.querySelector("path").style.fill = "#1F3641";
-      selectedMark.style.backgroundColor = "#A8BFC9";
+//change styling of selected mark
+function selectMark(selected) {
+      selected.querySelector("path").style.fill = "#1F3641";
+      selected.style.backgroundColor = "#A8BFC9";
 }
 
+//undo styling of mark when unselected
 function unselectMark(originalSelection) {
       originalSelection.querySelector("path").style.fill = "#A8BFC9";
       originalSelection.style.backgroundColor = "initial";
 }
 
-selectMark(selected);
+selectMark(selectedMark);
 
-
+//apply appropriate syling to selected mark
 for (let i = 0; i < markSelectors.length; i++) {
       markSelectors[i].onclick = () => {
-            if (selected !== markSelectors[i]) {
-                  unselectMark(selected);
-                  selected = markSelectors[i];
+            if (selectedMark !== markSelectors[i]) {
+                  unselectMark(selectedMark);
+                  selectedMark = markSelectors[i];
                   selectMark(markSelectors[i]);
             }
       }
 }
+
+
+
+
+
